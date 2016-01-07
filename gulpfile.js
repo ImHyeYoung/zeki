174a263b8a67b615f4d65f2371f9e8b89f247908
conrad <woosys25@gmail.com>
2016-01-07 15:16:14 +0900

    default tam
    Signed-off-by: conrad <woosys25@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
+'use strict';
+
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    gulpFilter = require('gulp-filter'),
+    jsmin = require('gulp-jsmin'),
+    uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync'),
     jade = require('gulp-jade'),
@@ -8,73 +13,93 @@ var gulp = require('gulp'),
 
 var src = {
     base: './',
-    app: 'public',
-    main: 'public/**/*.html',
-    docs: 'public/dist/docs/*.html',
-    scss: 'public/sass/**/*.scss',
-    jade: 'public/jade/**/*.jade'
+    app: 'app/',
+    main: 'app/public/**/*.html',
+    docs: 'app/public/dist/docs/*.html',
+    scss: 'app/public/sass/**/*.scss',
+    jade: 'app/public/jade/**/*.jade',
+    intro: 'app/public/*.jade',
+    js: 'app/js/*.js',
+    js_c: 'app/js/**/*.js',
+    dist: 'app/dist/'
 };
 
 // Compiles Sass files into CSS
-gulp.task('test', ['sassdoc','sass:compiles','sass:watch','resource','jade']);
-gulp.task('min_build', ['sassdoc','sass:build','resource','jade']);
+gulp.task('test', ['sass','resource','jade','js','js_c','intro','watch','sassdoc','server:start']);
+
+gulp.task('sass', function() {
+    return gulp.src(['./app/public/sass/stylesheet.scss'])
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest(src.dist + 'css'))
+        .pipe(reload({ stream:true }));
+});
+
+gulp.task('resource', function() {
+    gulp.src('./app/public/sass/resource/**')
+        .pipe(gulp.dest(src.dist + 'resource'));
+});
 
 gulp.task('jade', function() {
-    return gulp.src('./public/jade/**/*.jade')
+    //var filter = gulpFilter(['*', '!./app/public/jade/layout/*.jade']);
+    return gulp.src('./app/public/jade/**/*.jade')
+        //.pipe(filter)
         .pipe(jade({
             pretty: true
         })) // pip to jade plugin
-        .pipe(gulp.dest('./public/dist/html')) // tell gulp our output folder
+        .pipe(gulp.dest(src.dist + 'partials')) // tell gulp our output folder
         .pipe(reload({ stream:true }));
 });
 
-// test Compiles sass
-gulp.task('sass:compiles', function() {
-    return gulp.src(['./public/sass/stylesheet.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/dist/css'))
+gulp.task('js', function () {
+    gulp.src('./app/js/*.js')
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(src.dist + 'js'))
+});
+
+gulp.task('js_c', function () {
+    gulp.src('./app/js/**/*.js')
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(src.dist + 'js'))
+});
+
+gulp.task('intro', function() {
+    return gulp.src('./app/public/*.jade')
+        .pipe(jade({
+            pretty: true
+        })) // pip to jade plugin
+        .pipe(gulp.dest(src.dist)) // tell gulp our output folder
         .pipe(reload({ stream:true }));
 });
-gulp.task('resource', function() {
-    gulp.src('./public/sass/resource/**')
-        .pipe(gulp.dest('./public/dist/resource'));
-});
-gulp.task('sass:watch', function () {
-    browserSync({
-        port: 80,
-        server: {
-            baseDir: [src.base, src.app]
-        }
-    });
+
+gulp.task('watch', function () {
+    gulp.watch(src.scss, ['sass','resource','sassdoc']);
     gulp.watch(src.jade, ['jade','resource']);
+    gulp.watch(src.intro, ['intro','resource']);
+    gulp.watch(src.js, ['js','jade','intro']);
+    gulp.watch(src.js_c, ['js_c','jade','intro']);
     gulp.watch(src.main).on('change', reload);
     gulp.watch(src.docs).on('change', reload);
-    gulp.watch(src.scss, ['sass:compiles','resource','sassdoc']);
+    gulp.watch(src.intro).on('change', reload);
+    gulp.watch(src.js).on('change', reload);
+    gulp.watch(src.js_c).on('change', reload);
 });
 
-// build Compiles sass
-gulp.task('sass:build', function() {
-    return gulp.src(['./public/sass/stylesheet.scss'])
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./public/dist/css'))
+gulp.task('server:start', function() {
+    browserSync({
+        open: false,
+        notify: false,
+        port: 80,
+        server: {
+            baseDir: [src.dist]
+        }
+    })
 });
 
-// build Compiles jade
-gulp.task('jade', function() {
-    return gulp.src('./public/jade/**/*.jade')
-        .pipe(jade({
-            pretty: true
-        })) // pip to jade plugin
-        .pipe(gulp.dest('./public/dist/html')) // tell gulp our output folder
-        .pipe(reload({ stream:true }));
-});
-
-// build sass doc
 gulp.task('sassdoc', function () {
-
     var options = {
-        dest: './public/dist/docs',
+        dest: src.dist + 'docs',
         verbose: true,
         display: {
             access: ["private","public"],
@@ -85,9 +110,8 @@ gulp.task('sassdoc', function () {
         groups: {
             'undefined': 'general'
         },
-        basePath: 'https://github.com/changgyun/template/tree/master/public/sass'
+        basePath: 'https://github.com/conrad-ko/zeki'
     };
-
-    return gulp.src('./public/sass/**/*.scss')
+    return gulp.src('./app/public/sass/**/*.scss')
         .pipe(sassdoc(options))
 });
